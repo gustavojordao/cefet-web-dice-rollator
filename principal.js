3791dec98f530d497d43a06cde3ebfabf8821fbd
Gustavo Jordão Nunes <gustavo.jordao@hotmail.com.br>
2016-04-12 01:39:33 -0300

    Atividade Dice Rollator

--- principal.js
+++ principal.js
@@ -11,4 +11,57 @@
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
 
+function init(){
 
+  var i;
+  var j;
+
+  var dados = [4, 6, 8, 10, 12, 20];
+
+  var quantidades;
+
+  var rolados;
+
+  var resultado = document.getElementById('resultado');
+
+  var sorteio;
+
+  var soma;
+
+  var exibicao;
+
+  var rolar = document.getElementById('rolar');
+
+  rolar.addEventListener('click',
+    function(e){
+      soma = 0;
+      rolados = [];
+      quantidades = [];
+      for(i=0; i<dados.length; i++){
+        quantidades.push(document.getElementById('quantidadeD' + dados[i]).value);
+        for(j=0; j<quantidades[i]; j++){
+          sorteio = Math.floor(Math.random()*dados[i]) + 1;
+          soma = soma + sorteio;
+
+          rolados.push(sorteio);
+        }
+      }
+
+      exibicao = '';
+      for(i=0; i<rolados.length; i++){
+        exibicao = exibicao + rolados[i];
+        if(i != rolados.length-1){
+          exibicao = exibicao + ' + ';
+        }
+      }
+      exibicao = exibicao + ' = ' + soma;
+
+      resultado.textContent = exibicao;
+
+      resultado.parentNode.classList.remove('oculto');
+    }
+  );
+
+}
+
+init();
